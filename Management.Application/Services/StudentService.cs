c00acca94f31239468a816d00a2e0612a7e90f7f
Tohirbek <isomiddinovtohirjon5@gmail.com>
2025-12-15 09:51:23 -0700

    Array qo'shildi

---
+++ Management.Application/Services/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using Management.Infrastructure.Data;
+using ManagementDomain.Models;
+
+namespace Management.Application.Services
+{
+    public class StudentService
+    {
+        public DbContext DbContext { get; set; }
+
+        public StudentService()
+        {
+            this.DbContext = new DbContext();
+        }
+        public void AddStudent(string firstName, string lastName)
+        {
+            Student newStudent = new Student
+            {
+                Id = new Random().Next(1, 1000).ToString(),
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            this.DbContext.Students[0] = newStudent;
+        }
+    }
+}
